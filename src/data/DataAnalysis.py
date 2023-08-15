65b120b5e10833c7d3a0ba7f7068ae7091a24d41
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2023-08-15 16:26:32 +0300

    chore: update getTopRated

--- src/data/DataAnalysis.py
+++ src/data/DataAnalysis.py
@@ -6,6 +6,7 @@ import re
 data1 = pd.DataFrame(pd.read_csv('Transformed.csv'))
 
 
+
 def getTopRated():
 ## Identifier les 10 applications les plus populaires
     grouped = data1.groupby("Rating").sum()
@@ -18,42 +19,38 @@ def getTopRated():
 
 ## Suivre l'évolution de l'utilisation des applications en fonction de ses rating
 
-    top_installed_app =total_rates_ranked.groupby(total_rates_ranked.columns[1]).sum() 
+    top_installed_app = total_rates_ranked.groupby("Installs").sum() 
 
-    top_10_installed_app = top_installed_app.head(10)
+    top_10_installed_app = app_rating_ranking.head(20)
    
     top_10_installed_app.plot(kind='bar')
-    plt.ylabel('dernier mis à jour ')
-    plt.title('Évolution de l\'utilisation des 10 applications les plus notée en fonction de ses mises à jours')
-    #plt.show()
+    plt.ylabel('Installation')
+    plt.xlabel('Rating')
+    plt.title('Évolution des installations par rapport au note donnée par l\'utilisateur')
+    # plt.show()
 
 ## Etude de 4 cas de plus rated application pour savoir les avances données par les updates
-    grouped_by_app = total_rates_ranked.groupby("App").sum()
-    app1 = grouped_by_app["Boys Photo Editor - Six Pack & Men's Suit"]
-    app2 = grouped_by_app["Animated Photo Editor"]
-    app3 = grouped_by_app["ROBLOX"]
-    app4 = grouped_by_app["B Mobile Access"]
+    grouped_by_app = data1.groupby("App").sum()
+    app1 = grouped_by_app.loc["Boys Photo Editor - Six Pack & Men's Suit"]
+    app2 = grouped_by_app.loc["Animated Photo Editor"]
+    app3 = grouped_by_app.loc["ROBLOX"]
 
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(18, 9))
     plt.plot(app1, label='Boys Photo Editor - Six Pack & Men\'s Suit')
     plt.plot(app2, label='Animated Photo Editor')
-    plt.plot(app4, label='B Mobile Access')
     plt.plot(app3, label='ROBLOX')
     plt.ylabel('Rating per Application')
     plt.title('Évolution des nombres d\'installation selon les updates')
     plt.legend()
-    
-
-## Corrélation entre ces application
-    df = pd.DataFrame({'Boys Photo Editor - Six Pack & Men\'s Suit': app1, 'Animated Photo Editor': app2, 'B Mobile Access': app4,'ROBLOX' : app3})
-
-    corr = df.corr()
-
-    sb.heatmap(corr, annot=True, cmap='coolwarm')
-    plt.title('Corrélation entre les application les plus notéess')
     plt.show()
+## Corrélation entre ces application
+    # df = pd.DataFrame({'Boys Photo Editor - Six Pack & Men\'s Suit': app1, 'Animated Photo Editor': app2,'ROBLOX' : app3})
 
-## Conclusion
+    # corr = df.corr()
+
+    # sb.heatmap(corr, annot=True, cmap='coolwarm')
+    # plt.title('Corrélation entre les application les plus notéess')
+    # plt.show()
 
 def comparing_category_with_installation():
     category_installs = data1.groupby("Category")["Installs"].sum()
