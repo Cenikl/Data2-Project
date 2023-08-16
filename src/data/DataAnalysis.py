af32d1dea8b9594c24b0fd6b6517d070e1aa0791
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-08-16 13:50:31 +0300

    refactor: norms for column

--- src/data/DataAnalysis.py
+++ src/data/DataAnalysis.py
@@ -54,9 +54,9 @@ def comparing_category_with_installation(data):
 # Create a bar plot for the number of installations per category
     plt.figure(figsize=(10, 6))
     category_installs.plot(kind="bar")
-    plt.title("Nombre d'installations par catégorie d'application")
-    plt.xlabel("Catégorie")
-    plt.ylabel("Nombre d'installations")
+    plt.title("Number of installation per category")
+    plt.xlabel("Category")
+    plt.ylabel("Number of installation")
     plt.xticks(rotation=45, ha="right")
     plt.tight_layout()
 
@@ -72,7 +72,7 @@ def clean_price(price):
         return float(price.replace('$',''))
 
 def difference_between_paid_and_free_apps():
-    data1['Price'] = data1['Price'].apply(clean_price)
+    data1['Price'] = pd.to_numeric(data1['Price'], errors='coerce')
     data1['Installs'] = pd.to_numeric(data1['Installs'], errors='coerce')
     data1['Price'].fillna(0).astype(str)
     data1['Rating'] = data1['Rating'].replace(19.0,5.0)
@@ -104,3 +104,5 @@ def rating_apps_considering_prices():
     plt.show()
 
 rating_apps_considering_prices()
+comparing_category_with_installation(data1)
+difference_between_paid_and_free_apps()
